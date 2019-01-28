707565e4bc48401b4168a04c6bac224887e1d7bb
ses-rpx <ses_c@rpxcorp.com>
2019-01-28 09:44:03 +0530

    Add a full_title helper

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -9,7 +9,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get static_pages_home_url
     assert_response :success
-    assert_select "title", "Home | #{@title}"
+    assert_select "title", "#{@title}"
   end
 
   test "should get help" do
